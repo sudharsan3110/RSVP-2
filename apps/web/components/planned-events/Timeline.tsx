1d8221afbfb55eb6c0383efd374d27cfa0ddeeff
Anubhav Srivastava <159301654+DeltaDynamo@users.noreply.github.com>
2025-01-05 20:44:10 +0530

    Add navigation to event details page (#86)

--- apps/web/components/planned-events/Timeline.tsx
+++ apps/web/components/planned-events/Timeline.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import eventImageSrc from '@/public/images/demo-event-image.png';
 import { Icons } from '../common/Icon';
 import { IEvent } from '@/types/event';
+import Link from 'next/link';
 
 interface Event {
   title: string;
@@ -11,6 +12,7 @@ interface Event {
   location: string;
   attendees: string;
   isFree: boolean;
+  slug:string;
 }
 
 interface DateGroup {
@@ -29,6 +31,7 @@ const transformEventsToTimelineData = (events: IEvent[]) => {
     const formattedEvent: Event = {
       title: event.name,
       category: `${event.category}`,
+      slug: event.slug,
       time: `${new Date(event.startTime).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}, 
         ${new Date(event.startTime).toLocaleDateString('en-US', {
           month: 'short',
@@ -96,55 +99,57 @@ const ComicConTimeline = ({ events }: { events: IEvent[] | null }) => {
               </div>
 
               {dateGroup.events.map((event, eventIndex) => (
-                /* Container for each day */
-                <Card
-                  key={eventIndex}
-                  className="mb-4 flex flex-col-reverse justify-between gap-3 rounded-lg border border-dark-500 bg-dark-900 p-3 text-card-foreground shadow-sm md:h-auto md:min-h-[190px] md:flex-row"
-                >
-                  <div className="flex h-auto w-full flex-col justify-between gap-4 md:w-3/4">
-                    <div className="flex flex-col gap-2">
-                      <CardHeader className="flex flex-col items-start justify-between space-y-1.5 border-b-0 py-0 pl-0">
-                        <CardTitle className="text-xl font-bold leading-[25.2px] tracking-tight">
-                          {event.title}
-                        </CardTitle>
-                        <p className="text-base font-semibold leading-[19.6px]">{event.category}</p>
-                      </CardHeader>
+                <Link key={eventIndex} href={`/${event.slug}`}>
+                  {/* Container for each day */}
+                  <Card
+                    key={eventIndex}
+                    className="mb-4 flex flex-col-reverse justify-between gap-3 rounded-lg border border-dark-500 bg-dark-900 p-3 text-card-foreground shadow-sm md:h-auto md:min-h-[190px] md:flex-row hover:border-r-4 hover:border-b-4 hover:border-purple-500 transition-all duration-200 cursor-pointer"
+                  >
+                    <div className="flex h-auto w-full flex-col justify-between gap-4 md:w-3/4">
+                      <div className="flex flex-col gap-2">
+                        <CardHeader className="flex flex-col items-start justify-between space-y-1.5 border-b-0 py-0 pl-0">
+                          <CardTitle className="text-xl font-bold leading-[25.2px] tracking-tight">
+                            {event.title}
+                          </CardTitle>
+                          <p className="text-base font-semibold leading-[19.6px]">{event.category}</p>
+                        </CardHeader>
 
-                      <CardContent className="flex flex-col gap-1 p-0 pt-2">
-                        <p className="text-base font-bold leading-[19.6px]">{event.time}</p>
-                        <p className="text-sm font-medium leading-[19.6px]">{event.location}</p>
-                      </CardContent>
-                    </div>
-
-                    <div className="flex flex-row items-center justify-start gap-3 pt-2 text-sm font-semibold text-white">
-                      <div className="flex flex-row items-center justify-between gap-1 border-r-2 pr-3">
-                        {/* Tick */}
-                        <Icons.tick />
-                        <span className="text-[14px] font-semibold leading-[16.8px]">
-                          {event.attendees}
-                        </span>
+                        <CardContent className="flex flex-col gap-1 p-0 pt-2">
+                          <p className="text-base font-bold leading-[19.6px]">{event.time}</p>
+                          <p className="text-sm font-medium leading-[19.6px]">{event.location}</p>
+                        </CardContent>
                       </div>
 
-                      <div className="flex flex-row items-center justify-between gap-1">
-                        {/* Ticket Icon */}
-                        <Icons.ticket />
-                        <span className="text-[14px] font-semibold leading-[18px]">Free</span>
+                      <div className="flex flex-row items-center justify-start gap-3 pt-2 text-sm font-semibold text-white">
+                        <div className="flex flex-row items-center justify-between gap-1 border-r-2 pr-3">
+                          {/* Tick */}
+                          <Icons.tick />
+                          <span className="text-[14px] font-semibold leading-[16.8px]">
+                            {event.attendees}
+                          </span>
+                        </div>
+
+                        <div className="flex flex-row items-center justify-between gap-1">
+                          {/* Ticket Icon */}
+                          <Icons.ticket />
+                          <span className="text-[14px] font-semibold leading-[18px]">Free</span>
+                        </div>
                       </div>
                     </div>
-                  </div>
 
-                  <div className="relative min-h-[350px] w-full rounded-md md:h-auto md:min-h-[160px] md:min-w-[160px] md:max-w-[160px]">
-                    <div className="w-full overflow-hidden rounded-md">
-                      <Image
-                        src={eventImageSrc}
-                        alt={event.title}
-                        fill
-                        style={{ objectFit: 'cover' }}
-                        className="rounded-md"
-                      />
+                    <div className="relative min-h-[350px] w-full rounded-md md:h-auto md:min-h-[160px] md:min-w-[160px] md:max-w-[160px]">
+                      <div className="w-full overflow-hidden rounded-md">
+                        <Image
+                          src={eventImageSrc}
+                          alt={event.title}
+                          fill
+                          style={{ objectFit: 'cover' }}
+                          className="rounded-md"
+                        />
+                      </div>
                     </div>
-                  </div>
-                </Card>
+                  </Card>
+                </Link>
               ))}
             </div>
           </div>
