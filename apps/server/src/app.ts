cecbe098902b0ce400936447f2c9271d399b1e11
Anubhav Srivastava <159301654+DeltaDynamo@users.noreply.github.com>
2025-04-14 22:45:52 +0530

    Add a node-cron scheduler to run eventNotificationScript every hour (#227)
    * Add node-cron scheduler to run eventNotificationScript hourly
    
    * Add check to run cron job in production env

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,6 +1,8 @@
 import config from '@/config/config';
 import logger from '@/utils/logger';
 import { createServer } from './server';
+import cron from 'node-cron';
+import processEventNotifications from './scripts/eventNotificationScript';
 
 const port = config.PORT;
 const server = createServer();
@@ -8,3 +10,15 @@ const server = createServer();
 server.listen(port, () => {
   logger.info(`Api server running on ${port}`);
 });
+
+if(config.env==="production") {
+  cron.schedule('0 * * * *', async () => {
+    processEventNotifications()
+      .then(() => {
+        console.log(`Cron Job Triggered at ${new Date().toISOString()}`);
+      })
+      .catch((error) => {
+        console.error('Cron Job Error:', error);
+      });
+  });
+}
--- apps/server/src/scripts/eventNotificationScript.ts
+++ apps/server/src/scripts/eventNotificationScript.ts
@@ -29,86 +29,83 @@ class EmailService {
 }
 
 async function processEventNotifications() {
-  const currentTime = new Date();
-  currentTime.setMinutes(0, 0, 0);
+  try {
+    const currentTime = new Date();
+    currentTime.setMinutes(0, 0, 0);
 
-  const oneHourLaterFromCurrentTime = new Date(currentTime.getTime() + 3600000);
-  const twoHoursLaterFromCurrentTime = new Date(currentTime.getTime() + 7200000);
-  const twentyFourHoursLaterFromCurrentTime = new Date(currentTime.getTime() + 86400000);
-  const twentyFiveHoursLaterFromCurrentTime = new Date(currentTime.getTime() + 90000000);
+    const oneHourLaterFromCurrentTime = new Date(currentTime.getTime() + 3600000);
+    const twoHoursLaterFromCurrentTime = new Date(currentTime.getTime() + 7200000);
+    const twentyFourHoursLaterFromCurrentTime = new Date(currentTime.getTime() + 86400000);
+    const twentyFiveHoursLaterFromCurrentTime = new Date(currentTime.getTime() + 90000000);
 
-  const events = await prisma.event.findMany({
-    where: {
-      OR: [
-        { startTime: { gte: oneHourLaterFromCurrentTime, lt: twoHoursLaterFromCurrentTime } },
-        {
-          startTime: {
-            gte: twentyFourHoursLaterFromCurrentTime,
-            lt: twentyFiveHoursLaterFromCurrentTime,
+    const events = await prisma.event.findMany({
+      where: {
+        OR: [
+          { startTime: { gte: oneHourLaterFromCurrentTime, lt: twoHoursLaterFromCurrentTime } },
+          {
+            startTime: {
+              gte: twentyFourHoursLaterFromCurrentTime,
+              lt: twentyFiveHoursLaterFromCurrentTime,
+            },
+          },
+        ],
+        Attendee: {
+          some: {
+            hasAttended: false,
+            status: 'Going',
+            allowedStatus: true,
+            deleted: false,
           },
         },
-      ],
-      Attendee: {
-        some: {
-          hasAttended: false,
-          status: 'Going',
-          allowedStatus: true,
-          deleted: false,
-        },
       },
-    },
-    select: {
-      id: true,
-      name: true,
-      slug: true,
-      description: true,
-      startTime: true,
-      Attendee: {
-        where: {
-          hasAttended: false,
-          status: 'Going',
-          allowedStatus: true,
-          deleted: false,
-        },
-        select: {
-          id: true,
-          user: {
-            select: {
-              full_name: true,
-              primary_email: true,
+      select: {
+        id: true,
+        name: true,
+        slug: true,
+        description: true,
+        startTime: true,
+        Attendee: {
+          where: {
+            hasAttended: false,
+            status: 'Going',
+            allowedStatus: true,
+            deleted: false,
+          },
+          select: {
+            id: true,
+            user: {
+              select: {
+                full_name: true,
+                primary_email: true,
+              },
             },
           },
         },
       },
-    },
-  });
+    });
 
-  const emailPromises = events.map(async (event) => {
-    const eventEmailData = {
-      id: 6,
-      subject: `Your Event ${event.name} Starts Soon!`,
-      body: {
-        eventName: event.name,
-        updatesText: `Just a quick reminder that your event ${event.name} is starting soon at ${event.startTime}`,
-        updatesLink: `https://www.rsvp.kim/${event.slug}`,
-      },
-      bcc: event.Attendee.map(
-        (attendee: { user: { primary_email: string } }) => attendee.user.primary_email
-      ),
-    };
-    return EmailService.send(eventEmailData);
-  });
+    const emailPromises = events.map(async (event) => {
+      const eventEmailData = {
+        id: 6,
+        subject: `Your Event ${event.name} Starts Soon!`,
+        body: {
+          eventName: event.name,
+          updatesText: `Just a quick reminder that your event ${event.name} is starting soon at ${event.startTime}`,
+          updatesLink: `https://www.rsvp.kim/${event.slug}`,
+        },
+        bcc: event.Attendee.map(
+          (attendee: { user: { primary_email: string } }) => attendee.user.primary_email
+        ),
+      };
+      return EmailService.send(eventEmailData);
+    });
 
-  await Promise.all(emailPromises);
+    await Promise.all(emailPromises);
+  } catch (error) {
+    throw error;
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-processEventNotifications()
-  .then(() => {
-    console.log(`Events Notification Sent Successfully at ${new Date()}`);
-  })
-  .catch((error) => {
-    console.error('Some Error Occurred while sending Event Notification!', error);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+export default processEventNotifications;
